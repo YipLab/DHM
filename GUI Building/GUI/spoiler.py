4e5339ab43dcbd9c27ed3362088c382647e6f219
Ziyang Yu <deanziyangyu@gmail.com>
2022-07-21 21:32:37 -0400

    Integrated dynamic spoiler into GUI

--- GUI Building/GUI/spoiler.py
+++ GUI Building/GUI/spoiler.py
@@ -3,32 +3,26 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
 
 
 class Spoiler(QWidget):
-    def __init__(self, parent=None, title='', animationDuration=300, addRemoveOption='None'):
+    def __init__(self, parent=None, ui_path = '', animationDuration=300):
         super(Spoiler, self).__init__(parent=parent)
-        uic.loadUi('./ui_files/spoiler_widget.ui', self)  # Load the .ui file
+        uic.loadUi(ui_path, self)  # Load the .ui file
 
         self.animationDuration = animationDuration
         self.toggleAnimation = QParallelAnimationGroup()
-        # self.contentArea = QScrollArea()
-        # self.headerLine = QFrame()
-        # self.toggleButton = QToolButton()
-        self.mainLayout = QGridLayout()
-        # self.childWidgets = []
 
         self.toggleButton.setStyleSheet("QToolButton { border: none; }")
         self.toggleButton.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
         self.toggleButton.setArrowType(Qt.RightArrow)
-        self.toggleButton.setText(str(title))
         self.toggleButton.setCheckable(True)
         self.toggleButton.setChecked(False)
 
-        self.addRemoveOperation = addRemoveOption
-
         self.contentArea.setStyleSheet("QScrollArea { background-color: white; border: none; }")
-        # self.contentArea.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         # start out collapsed
         self.contentArea.setMaximumHeight(0)
         self.contentArea.setMinimumHeight(0)
@@ -37,13 +31,10 @@ class Spoiler(QWidget):
         self.toggleAnimation.addAnimation(QPropertyAnimation(self, b"maximumHeight"))
         self.toggleAnimation.addAnimation(QPropertyAnimation(self.contentArea, b"maximumHeight"))
         # don't waste space
-        # self.mainLayout.setVerticalSpacing(0)
-        # self.mainLayout.setContentsMargins(0, 0, 0, 0)
-        # self.mainLayout.addWidget(self.toggleButton, 0, 0, 1, 1, Qt.AlignLeft)
-        # if addRemoveOption is not 'None':
-        #     self.mainLayout.addWidget(self.addRemoveButton, 0, 2, 1, 1, Qt.AlignRight)
-        # self.mainLayout.addWidget(self.contentArea, 1, 0, 1, 3)
-        # self.setLayout(self.mainLayout)
+        self.mainLayout.setVerticalSpacing(0)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.mainLayout.addWidget(self.toggleButton, 0, 0, 1, 1, Qt.AlignLeft)
+        self.setLayout(self.mainLayout)
 
         def start_animation(checked):
             arrow_type = Qt.DownArrow if checked else Qt.RightArrow
@@ -53,21 +44,15 @@ class Spoiler(QWidget):
             self.toggleAnimation.start()
 
         self.toggleButton.clicked.connect(start_animation)
-        self.contentLayout = QVBoxLayout()
-        print (self.sizeHint())
-        print (self.contentLayout.sizeHint())
-        # self.contentArea.setMaximumHeight(300 if self.contentLayout.sizeHint().height() < 300 else self.contentLayout.sizeHint().height())
+        self.pushButton_Confirm.clicked.connect(start_animation)
         self.setContentLayout(self.contentLayout)
-
 
     def setContentLayout(self, contentLayout):
         self.contentArea.destroy()
         self.contentArea.setLayout(contentLayout)
-        # print (self.sizeHint())
         collapsedHeight = self.sizeHint().height() - self.contentArea.maximumHeight()
-        # contentHeight = 400 if contentLayout.sizeHint().height() > 400 else contentLayout.sizeHint().height()
         contentHeight = contentLayout.sizeHint().height()
-        # print (contentLayout.sizeHint())
+
         for i in range(self.toggleAnimation.animationCount()-1):
             spoilerAnimation = self.toggleAnimation.animationAt(i)
             spoilerAnimation.setDuration(self.animationDuration)
@@ -78,29 +63,41 @@ class Spoiler(QWidget):
         contentAnimation.setStartValue(0)
         contentAnimation.setEndValue(contentHeight)
 
+class set_param_Spoiler(Spoiler):
+    def __init__(self, parent=None, ui_path = '', animationDuration=300):
+        super(set_param_Spoiler, self).__init__(parent=parent, ui_path = ui_path, animationDuration=animationDuration)
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        # uic.loadUi('./ui_files/std_main_alpha.ui', self)  # Load the .ui file
+        uic.loadUi('./ui_files/std_main_alpha.ui', self)  # Load the .ui file
         self.init_ui()
 
     def init_ui(self):
 
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowIcon(QIcon('web.png'))
+        # self.setGeometry(300, 300, 300, 220)
 
-        self.centralWidget = QFrame()
-        self.centralLayout = QVBoxLayout()
-        self.centralWidget.setLayout(self.centralLayout)
+        # self.scroll = QScrollArea()             # Scroll Area which contains the widgets, set as the centralWidget
+        # self.widget = QWidget()                 # Widget that contains the collection of Vertical Box
+        # self.vbox = QVBoxLayout()
+        self.img_canvas = FigureCanvas(plt.Figure())
+        self.img_canvas.minimumWidth = 800
+        self.img_canvas.minimumHeight = 600
+        self.bar_layout.addWidget(NavigationToolbar(self.img_canvas, self))
+        self.figure_layout.addWidget(self.img_canvas)
 
-        self.spoiler1 = Spoiler(addRemoveOption='None', title='Group 1')
-        self.spoiler2 = Spoiler(addRemoveOption='None', title='Group 2')
+        self.spoiler1 = set_param_Spoiler(ui_path ='./ui_files/spoiler_param_set_widget.ui')
+        self.spoiler2 = Spoiler(ui_path='./ui_files/spoiler_template_widget.ui')
+        self.spoiler3 = Spoiler(ui_path='./ui_files/spoiler_template_widget.ui')
+        self.vbox.addWidget(self.spoiler1)
+        self.vbox.addWidget(self.spoiler2)
+        self.vbox.addWidget(self.spoiler3)
 
-        self.centralLayout.addWidget(self.spoiler1)
-        self.setCentralWidget(self.centralWidget)
-        self.centralLayout.addWidget(self.spoiler2)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+
+        # self.setCentralWidget(self.scroll)
         self.show()
 
 import sys
@@ -108,5 +105,9 @@ import sys
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
+    # File = open('./ui_files/styles/Yip_lab.qss', 'r')
+    # with File:
+    #     qss_style = File.read()
+    #     app.setStyleSheet(qss_style)
     ex = MainWindow()
     sys.exit(app.exec_())
